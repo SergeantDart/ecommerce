279cac4ea4370fd3ac1240baa3a45924d7344b6e
Dumitrascu Antonio - Sebastian <dumitrascuantonio18@stud.ase.ro>
2022-10-16 23:50:47 +0300

    -- init microservices and config services

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Collections.Concurrent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+
+namespace Infrastructure.Data
+{
+  public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
